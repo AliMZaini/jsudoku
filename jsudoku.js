d70376fd6ae4eec02503ee44e082b7bc68a200c2
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-03-31 16:46:19 +0100

    Checks now take the baord as a parameter. Added method to find empty index

--- jsudoku.js
+++ jsudoku.js
@@ -1,20 +1,21 @@
+// From: https://dingo.sbs.arizona.edu/~sandiway/sudoku/examples.html
 var originalBoard = [
-      , , ,2,6, ,7, ,1
-    ,6,8, , ,7, , ,9,
-    ,1,9, , , ,4,5, ,
-    ,8,2, ,1, , , ,4,
-    , , ,4,6, ,2,9, ,
-    , ,5, , , ,3, ,2,8
-    , , ,9,3, , , ,7,4
-    , ,4, , ,5, , ,3,6
-    ,7, ,3, ,1,8, , ,undefined
+     0,0,0,2,6,0,7,0,1
+    ,6,8,0,0,7,0,0,9,0
+    ,1,9,0,0,0,4,5,0,0
+    ,8,2,0,1,0,0,0,4,0
+    ,0,0,4,6,0,2,9,0,0
+    ,0,5,0,0,0,3,0,2,8
+    ,0,0,9,3,0,0,0,7,4
+    ,0,4,0,0,5,0,0,3,6
+    ,7,0,3,0,1,8,0,0,0
 ];
 
 var board = originalBoard.slice();
 
-function printBoard() {
+function printBoard(board) {
     for (var i = 0; i < 9; i++) {
-        console.log(i, getRow(i));
+        console.log(i, getRow(i, board));
     }
 }
 
@@ -25,9 +26,8 @@ function getOccurances(array, query) {
 // checks there are no duplicates in an array, where duplicate nulls are allowed
 function checkNoDuplicates(array){
     for (i = 0; i < array.length; i++){
-        if(array[i] != null){
+        if(array[i] != 0){
             if (getOccurances(array, array[i]) != 1){
-                console.log(array);
                 return false;
             }
         }
@@ -35,50 +35,50 @@ function checkNoDuplicates(array){
     return true;
 }
 
-function getRow(rowNumber) {
+function getRow(rowNumber, board) {
     var rowNumber = rowNumber * 9;
     return board.slice(rowNumber, rowNumber + 9);
 }
 
-function checkRow(rowNumber){
-    var row = getRow(rowNumber);
+function checkRow(rowNumber, board){
+    var row = getRow(rowNumber, board);
     return checkNoDuplicates(row);
 }
 
-function getColumn(colNumber){
+function getColumn(colNumber, board){
     var column = [];
     for (i = 0; i < 9; i++){
-        var row = getRow(i);
+        var row = getRow(i, board);
         column.push(row[colNumber]);
     }
     return column;
 }
 
-function checkColumn(colNumber){
-    var column = getColumn(colNumber);
+function checkColumn(colNumber, board){
+    var column = getColumn(colNumber, board);
     return checkNoDuplicates(column);
 }
 
 // disgusting
-function getSubgrid(gridNumber){
+function getSubgrid(gridNumber, board){
     var subGrid = [];
 
     var gridRows = [];
 
     if([0, 1, 2].includes(gridNumber)){
-        gridRows.push(getRow(0));
-        gridRows.push(getRow(1));
-        gridRows.push(getRow(2));
+        gridRows.push(getRow(0, board));
+        gridRows.push(getRow(1, board));
+        gridRows.push(getRow(2, board));
     }
     if([3, 4, 5].includes(gridNumber)){
-        gridRows.push(getRow(3));
-        gridRows.push(getRow(4));
-        gridRows.push(getRow(5));
+        gridRows.push(getRow(3, board));
+        gridRows.push(getRow(4, board));
+        gridRows.push(getRow(5, board));
     }
     if([6, 7, 8].includes(gridNumber)){
-        gridRows.push(getRow(6));
-        gridRows.push(getRow(7));
-        gridRows.push(getRow(8));
+        gridRows.push(getRow(6, board));
+        gridRows.push(getRow(7, board));
+        gridRows.push(getRow(8, board));
     }
 
     if([0, 3, 6].includes(gridNumber)){
@@ -99,30 +99,33 @@ function getSubgrid(gridNumber){
     return subGrid;
 }
 
-function checkSubgrid(gridNumber){
-    var subGrid = getSubgrid(gridNumber);
+function checkSubgrid(gridNumber, board){
+    var subGrid = getSubgrid(gridNumber, board);
     return checkNoDuplicates(subGrid);
 }
 
 // returns true if the board is in a valid state
-function checkBoard(){
+function checkBoard(board){
     for (var i = 0; i < 9; i++){
-        if(!checkRow(i) || !checkColumn(i) || !checkSubgrid(i)){
+        if(!checkRow(i, board) || !checkColumn(i, board) || !checkSubgrid(i, board)){
             return false;
         }
     }
     return true;
 }
 
-function insertValue(value, columnNumber, rowNumber){
-    // we will add a check to ensure this isn't overriding the initial board
-    if(originalBoard[(9 * columnNumber) + rowNumber] === undefined){
-        board[9 * columnNumber + rowNumber] = value;
-        return true;
+function findEmpty(board) {
+    for (var i = 0; i < board.length; i++){
+        if (board[i] === 0){
+            return i;
+        }
     }
+    return -1;
+}
+
+function solve(board){
     return false;
 }
 
-// solution is found if all cells have values and checkBoard returns true. we do this check when we are on the final cell.
-
-console.log(checkBoard());
+solve(board);
+printBoard(board);
